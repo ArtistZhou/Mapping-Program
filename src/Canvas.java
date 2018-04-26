2bcbc00bbce9c6f30410ee0ac08f5e848763fe9b
akirarsah <akirarsah@gmail.com>
2018-04-26 17:42:10 -0400

    created new test class

--- src/Canvas.java
+++ src/Canvas.java
@@ -14,16 +14,7 @@ public class Canvas extends JPanel {
 	}
 
 	public static void main(String[] args) {
-		Graph g = new Graph("nys.txt");
-		System.out.println("done making graph");
-		System.out.println("Latitude ranges from " + g.minlat + " to " + g.maxlat);
-		System.out.println("Longitude ranges from " + g.minlon + " to " + g.maxlon);
-		List<Node> n = g.shortestPath("i0", "i100");
-		System.out.println("done finding shortest path");
-		System.out.println(n.toString());
-		for(Node node: n) {
-			System.out.println(node.lat + " " + node.lon);
-		}
+	
 	}
 
 	public static double generateX(Graph g, Node n) {
