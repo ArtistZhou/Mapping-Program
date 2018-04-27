339ed0bef83899f1fffc229c5bd6ff399689d92c
akirarsah <akirarsah@gmail.com>
2018-04-27 00:38:10 -0400

    some more changes

--- src/Canvas.java
+++ src/Canvas.java
@@ -6,19 +6,26 @@ import java.awt.Graphics;
 import java.util.List;
 
 public class Canvas extends JPanel {
-	final static double offset = 0.05; //boundary of map
+	final static double offset = 0.05; // boundary of map
 	double length;
 	double width;
 	Graph graph;
 	double scale;
-	
-	//main class has a JFrame that adds this canvas
+
+	// main class has a JFrame that adds this canvas
 	public Canvas(Graph g) {
 		this.graph = g;
-		if(graph.maxlon - graph.minlon > graph.maxlat-graph.minlat) {
+		// the idea is that I am using one scale for both x and y such that neither x
+		// nor y exceeds length or width
+		if (graph.maxlon - graph.minlon > graph.maxlat - graph.minlat) {
 			scale = graph.maxlon - graph.minlon;
-		}else {
-			scale = graph.maxlat-graph.minlat;
+		} else {
+			scale = graph.maxlat - graph.minlat;
+		}
+		if (getWidth() > getHeight()) {
+			length = width = getHeight();
+		} else {
+			length = width = getWidth();
 		}
 		repaint();
 	}
@@ -29,54 +36,47 @@ public class Canvas extends JPanel {
 		// if the shortest path is known, show that too
 	}
 
+	// added some math to center the map on the canvas
 	public double generateX(Node n) {
-		return ((n.lon-graph.minlon)/scale*width);
-	}	
-
-	public double generateY(Node n) {
-		return (length - (n.lat-graph.minlat)/scale*length);
+		double realx = (n.lon - graph.minlon) / scale * width;
+		if (getWidth() > getHeight()) {
+			return (realx + (double) (getWidth() - getHeight()) / 2);
+		}
+		return realx;
 	}
 
-	
+	public double generateY(Node n) {
+		double realy = (1 - (n.lat-graph.minlat)/scale)*length;
+		if(getHeight()>getWidth()) {
+			return ( realy + (double)(getHeight()-getWidth())/2);
+		}
+		return realy;
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
-		//aspect ratio = width/height
-//		double ratio = (graph.maxlon - graph.minlon)/(graph.maxlat - graph.minlat);
-//		int LENGTH = getHeight();
-//		int WIDTH = getWidth();
-//		double ratio_of_window = WIDTH/LENGTH;
-//		
-//		if(ratio_of_window != ratio) {
-//			if(LENGTH > WIDTH) {
-//				width = getWidth();
-//				length = ratio*getWidth();
-//			} else {
-//				length = getHeight();
-//				width = ratio*getHeight();
-//			}
-//		}
-		if(getWidth()>getHeight()) {
+		if (getWidth() > getHeight()) {
 			length = width = getHeight();
-		}else {
+			g.drawLine((getWidth() - getHeight()) / 2, 0, (getWidth() - getHeight())/2, getHeight());
+			g.drawLine(getWidth()-(getWidth() - getHeight()) / 2, 0, getWidth()-(getWidth() - getHeight())/2, getHeight());
+		} else {
 			length = width = getWidth();
+			g.drawLine(0, (getHeight() - getWidth()) / 2, getWidth(), (getHeight() - getWidth()) / 2);
+			g.drawLine(0, getHeight()-(getHeight() - getWidth()) / 2, getWidth(), getHeight() - (getHeight() - getWidth()) / 2);
 		}
 
-		//for each intersection in list of vertices in graph
-		for(String s: graph.vertices.keySet()) {
+		// for each intersection in list of vertices in graph
+		for (String s : graph.vertices.keySet()) {
 			Node node = graph.vertices.get(s);
-			int x1 = (int)generateX(node);
-			int y1 = (int)generateY(node);
-			
-			for(Edge adj : node.edgeList) {
+			int x1 = (int) generateX(node);
+			int y1 = (int) generateY(node);
+
+			for (Edge adj : node.edgeList) {
 				Node destination = adj.dest;
-				int x2 = (int)generateX(destination);
-				int y2 = (int)generateY(destination);
+				int x2 = (int) generateX(destination);
+				int y2 = (int) generateY(destination);
 				g.drawLine(x1, y1, x2, y2);
 			}
 		}
-		
-		//System.out.println("\n drew " + count + " edges, vertices = " + graph.vertices.size());
-		//draw line from that vertex to each neighbour
-	
 	}
 }
